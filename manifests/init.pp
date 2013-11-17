80fbac6849f4ad5ae8f0c4a1b4370d196b430255
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-11-17 21:49:05 +0900

    Add bowtie class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Installs Bowtie
+#
+# Params:
+#
+#   version -- the version of bowtie
+
+class bowtie(
+  $version = '1.5'
+) {
+  package { 'bowtie':
+    source   => "http://bowtieapp.com/bowtie-${version}.zip",
+    provider => 'compressed_app'
+  }
 }
